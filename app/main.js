6ff8964e0f4c61d063a28a84e3aa569014dc7cae
Krittiya Clark <krittiyaclark@gmail.com>
2018-06-01 19:32:20 -0400

    Test append an error message on click

--- app/main.js
+++ app/main.js
@@ -1,32 +1,51 @@
 // Handlers
 // Select the todo form input
-const addTodo = document.getElementById(add-todo);
+const addTodo = document.getElementById(add-todo).value;
 // Select the value form input
-const addValue = document.getElementById(add-value);
+const addValue = document.getElementById(add-value).value;
 // Select the button
 const submitButton = document.getElementById(submit-button);
 // Select the plus sign
 const plus = document.getElementById(plus);
 // Select the minus sign
 const minus = document.getElementById(minus);
-
+// Select p for an error message
+const error = document.getElementById(error);
 // Todo object
-let todoList = {
+const todoList = {
   // Store todo from input in an array
-  todos: [],
-  // Display todo method
-  displayTodos: function() {
-    // Check if the todo array is empty
-    if (this.todos.length === 0) {
-      console.log('Please add your todo list!');
-      //error.innerHTML = 'Please add your todo list!';
-    // } else {
-          // Loop through the todo array
-    //   for (let i = 0; i < this.todos.length; i++) {
-    //     if (this.todos[i].complete === true) {
-    //       this.todos[i].append();
-    //     }
-    //   }
-    // }
-  }
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+        // displayTodos() {
+        //   // Check if the todo array is empty
+        //   if (this.allItems.exp.length === 0) {
+        //     console.log('Please add your todo list!');
+        //     error.innerHTML = 'Please add your todo list!';
+        //   // } else {
+        //         // Loop through the todo array
+        //   //   for (let i = 0; i < this.todos.length; i++) {
+        //   //     if (this.todos[i].complete === true) {
+        //   //       this.todos[i].append();
+        //   //     }
+        //   //   }
+        //   // }
+        //   }
+        //  }
+       };
+// Display todo function
+function displayTodos(value) {
+       if (addValue === null) {
+        console.log('Please add your todo list!');
+        error.innerHTML = 'Please add your todo list!';
 }
+}
+
+displayTodos();
+// Button on click
+submitButton.document.addEventListener('click', displayTodos);
