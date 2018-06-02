9804eec96048619bb85ecc800e8d00c35597203a
Krittiya Clark <krittiyaclark@gmail.com>
2018-06-02 18:48:07 -0400

    Setup Event Listeners

--- app/main.js
+++ app/main.js
@@ -19,32 +19,46 @@ const UIController = (function () {
 })();
 
 // Globel Controller
-const controler = (function (budgetCtrl, UICtrl) {
+const controller = (function (budgetCtrl, UICtrl) {
+
+  // Set up all eventlisteners
+  const setupEventleners = function () {
+    // Select the button
+    const submitButton = document.querySelector('#submit-button');
+    // On Click
+    submitButton.addEventListener('click', ctrlAddItem);
+    // Set up keypress when user click enter
+    document.addEventListener('keypress', (e) => {
+      // Keypress Code, keyCode and Keypress Code for older browser which
+      if (e.keyCode === 13 || e.which === 13) {
+        console.log('Keypress!');
+        ctrlAddItem();
+      }
+    });
+  };
+
   // Add item
   let ctrlAddItem = () => {
     // 1. Get the field input data
-    let input = UICtrl.getInput();
+    const input = UICtrl.getInput();
     console.log(input);
-    // 2. Add the item to the budget controler
+    // 2. Add the item to the budget controller
 
     // 3. Add the item to the UI
 
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-    console.log('It works!');
-  }
+  };
 
-  // Select the button
-  const submitButton = document.querySelector('#submit-button');
-  // On Click
-  submitButton.addEventListener('click', ctrlAddItem);
-  // Set up keypress when user click enter
-  document.addEventListener('keypress', (e) => {
-    // Keypress Code, keyCode and Keypress Code for older browser which
-    if (e.keyCode === 13 || e.which === 13) {
-      console.log('Keypress!');
-      ctrlAddItem();
+  // Call setupEventleners() / Public function
+  return {
+    init: function () {
+      console.log('App started!');
+      setupEventleners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+// Start the app
+controller.init();
